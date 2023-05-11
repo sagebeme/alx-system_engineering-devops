d810fdbbbfee51c048e23016e5a34912b70eb42b
sagebeme <eudicas5k@gmail.com>
2023-05-11 23:54:27 +0300

    pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,6 @@ def number_of_subscribers(subreddit):
     """
     headers = {"User-Agent": 'my_bot/0.0.1'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
