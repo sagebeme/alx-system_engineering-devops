34800f2a6d3aadd067173e65aef4816cda2ad8e9
sagebeme <eudicas5k@gmail.com>
2023-05-11 23:15:48 +0300

    gets the number of subs in redit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Model working with Reddit API
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Gets the number of subscribers of reddit
+    """
+    headers = {"User-Agent" : 'my_bot/0.0.1'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
