1682613577970a6140321d833382d17549fb8d67
sagebeme <eudicas5k@gmail.com>
2023-05-11 23:36:48 +0300

    prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,6 +3,20 @@
 Working with Reddit API
 """
 
+import requests
 
 
 def top_ten(subreddit):
+    """
+    prints the titles of the first 10
+    hot posts listed for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {"User-Agent": "DepartureNo8863"}
+    response = requests.get(url, headers=headers, 
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
